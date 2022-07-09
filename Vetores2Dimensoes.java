32c471438d1aeaa470a45238703d74617bce2728
Miriã Batista da Cruz <106412260+MiriaCruz@users.noreply.github.com>
2022-07-09 20:38:19 -0300

    Vetores

---
+++ Vetores2Dimensoes.java
@@ -0,0 +1,26 @@
+
+public class Vetores2Dimensoes {
+	
+	public static void main(String[] args) {
+		Double faturamentoDia = 1000.0;
+		
+		Double[] faturamentoJaneiro = new Double[] { 1500.0, 2000.0, 1700.0 };
+		Double[] faturamentoFevereiro = new Double[] { 1000.0, 2500.0, 1800.0 };
+		
+		Double[][] faturamentoAnual = new Double[][] { faturamentoJaneiro, faturamentoFevereiro };
+		
+//		System.out.println("Faturamento do dia 1º de Janeiro: " + faturamentoAnual[0][0]);
+//		System.out.println("Faturamento do dia 3 de Fevereiro: " + faturamentoAnual[1][2]);
+		
+	
+			
+			Double[] mes = faturamentoAnual[i];
+			
+			for(int y = 0; y < mes.length; y++) {
+				Double dia = mes[y]; // Double dia = faturamentoAnual[i][y];
+				
+				System.out.println("Dia " + (y + 1) + ": " + dia);
+			}
+		}
+	}
+}   
